d8ead75056254256c675ec397a65101a463b7002
Smruthul12 <msmruthul2002@gmail.com>
2024-07-17 23:39:05 +0530

    trying to deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Earth_react-three-fiber/",
   plugins: [react()],
-  base: "/vite-deploy/"
 })
